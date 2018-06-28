6c40f7c459928f4d75a85e82492cf2ff1d23bc3e
Mike Rathbun <mikerathbun@me.com>
2018-06-28 15:07:41 -0500

    more

--- src/app/city-search/city-search.component.ts
+++ src/app/city-search/city-search.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms'
+import { Component, OnInit } from '@angular/core'
+import { FormControl, Validators } from '@angular/forms'
 import { WeatherService } from '../weather/weather.service'
+import { debounceTime } from 'rxjs/operators'
 
 
 @Component({
@@ -9,14 +10,15 @@ import { WeatherService } from '../weather/weather.service'
   styleUrls: ['./city-search.component.css']
 })
 export class CitySearchComponent implements OnInit {
-  search = new FormControl()
+  search = new FormControl('', [Validators.minLength(2)])
 
   constructor(private weatherService: WeatherService) { }
 
   ngOnInit() {
     this.search.valueChanges
+      .pipe(debounceTime(1000))
       .subscribe((searchValue: string) => {
-        if (searchValue) {
+        if (!this.search.invalid) {
           const userInput = searchValue.split(',').map(s => s.trim())
           this.weatherService.getCurrentWeather(
             userInput[0],
